f5662c71e2c64f706aa9bb8913d42cf2c358d417
Denys <denys.hudymov@gmail.com>
2023-09-20 20:13:02 +0300

    main class that runs a console interface for deque and in the future will run multi-list

---
+++ lab1/src/main/java/org/denys/hudymov/App.java
@@ -0,0 +1,9 @@
+package org.denys.hudymov;
+
+public class App {
+
+    public static void main(String[] args){
+        ConsoleInterface consoleInterface = new ConsoleInterface();
+        consoleInterface.run();
+    }
+}
