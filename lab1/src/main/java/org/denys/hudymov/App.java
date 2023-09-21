d8591280af3ab072797b9dac31ad8577fe76e965
Denys <denys.hudymov@gmail.com>
2023-09-21 23:55:19 +0300

    Was changed package due to moving to deque package all related stuff

--- lab1/src/main/java/org/denys/hudymov/App.java
+++ lab1/src/main/java/org/denys/hudymov/App.java
@@ -1,8 +1,10 @@
 package org.denys.hudymov;
 
+import org.denys.hudymov.deque.ConsoleInterface;
+
 public class App {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         ConsoleInterface consoleInterface = new ConsoleInterface();
         consoleInterface.run();
     }
