eb2b183a175b59c9407979d5ecb26ae3a609ee70
Denys <denys.hudymov@gmail.com>
2023-09-22 23:07:41 +0300

    MultiList has been implemented all methods of the GenericTree Interface

--- lab1/src/main/java/org/denys/hudymov/multilist/MultiList.java
+++ lab1/src/main/java/org/denys/hudymov/multilist/MultiList.java
@@ -1,10 +1,13 @@
 package org.denys.hudymov.multilist;
 
-import lombok.*;
-import org.apache.commons.lang3.tuple.Pair;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.Objects;
-import java.util.Optional;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Data
@@ -18,12 +21,39 @@ public class MultiList implements GenericTree<Integer> {
 
     @Override
     public int countElements() {
-        return 0;
+        if (getRoot() == null) {
+            return 0;
+        }
+        AtomicInteger count = new AtomicInteger(0);
+        var node = traversal(-1, getRoot(), count);
+        return count.get();
     }
 
     @Override
     public int countSizeOfLayer(int layerNumber) {
-        return 0;
+        if (getRoot() == null || layerNumber < 0) {
+            return 0;
+        }
+
+        AtomicInteger count = new AtomicInteger(0);
+        countSizeOfLayer(getRoot(), layerNumber, 0, count);
+        return count.get();
+    }
+
+    private void countSizeOfLayer(Node<Integer> node, int targetLayer, int currentLayer, AtomicInteger count) {
+        if (currentLayer == targetLayer) {
+            count.incrementAndGet();
+        }
+
+        // Recursively check child nodes
+        if (node.getChild() != null) {
+            countSizeOfLayer(node.getChild(), targetLayer, currentLayer + 1, count);
+        }
+
+        // Recursively check next sibling nodes at the same level
+        if (node.getNext() != null) {
+            countSizeOfLayer(node.getNext(), targetLayer, currentLayer, count);
+        }
     }
 
     @Override
@@ -63,70 +93,134 @@ public class MultiList implements GenericTree<Integer> {
 
     @Override
     public void deleteLayerWithChildren(int layerNumber) {
+        if (getRoot() == null || layerNumber <= 0) {
+            setRoot(null);
+            return;
+        }
 
+        deleteLayerWithChildren(getRoot(), layerNumber - 1, 0);
+    }
+
+    private void deleteLayerWithChildren(Node<Integer> node, int targetLayer, int currentLayer) {
+        if (currentLayer == targetLayer) {
+            node.setChild(null);
+        }
+
+        // Recursively check child nodes
+        if (node.getChild() != null) {
+            deleteLayerWithChildren(node.getChild(), targetLayer, currentLayer + 1);
+        }
+
+        // Recursively check next sibling nodes at the same level
+        if (node.getNext() != null) {
+            deleteLayerWithChildren(node.getNext(), targetLayer, currentLayer);
+        }
     }
 
     @Override
-    public void deleteBranch(int branchIndex) {
+    public void deleteBranch(int index, boolean deleteChild) throws IndexOutOfBoundsException {
+        if (getRoot() == null || index < 0) {
+            setRoot(null);
+            return;
+        }
 
+        AtomicInteger count = new AtomicInteger(-1);
+        deleteBranch(getRoot(), index, count, deleteChild);
+        if (count.get() != index) {
+            throw new IndexOutOfBoundsException("This index {" + index + "} doesn't exist. Maximum index is {" + count + "}");
+        }
+    }
+
+    private void deleteBranch(Node<Integer> node, int targetIndex, AtomicInteger currentIndex, boolean deleteChild) {
+        currentIndex.incrementAndGet();
+
+        if (targetIndex == currentIndex.get() && deleteChild) {
+            node.setChild(null);
+            return;
+        } else if (targetIndex == currentIndex.get()) {
+            node.setNext(null);
+            return;
+        }
+
+        // Recursively check child nodes
+        if (node.getChild() != null) {
+            deleteBranch(node.getChild(), targetIndex, currentIndex, deleteChild);
+        }
+
+        // Recursively check next sibling nodes at the same level
+        if (node.getNext() != null) {
+            deleteBranch(node.getNext(), targetIndex, currentIndex, deleteChild);
+        }
     }
 
     @Override
-    public void copy() {
+    public MultiList copy() {
+        if (getRoot() == null) {
+            return new MultiList(); // Return an empty multi-list
+        }
 
+        Node<Integer> copiedRoot = copyTree(getRoot());
+        MultiList copiedMultiList = new MultiList();
+        copiedMultiList.setRoot(copiedRoot);
+
+        return copiedMultiList;
+    }
+
+    private Node<Integer> copyTree(Node<Integer> originalNode) {
+        var copiedNode = Node.<Integer>builder()
+                .value(originalNode.getValue())
+                .build();
+
+        // Recursively copy child nodes
+        if (originalNode.getChild() != null) {
+            Node<Integer> copiedChild = copyTree(originalNode.getChild());
+            copiedNode.setChild(copiedChild);
+        }
+
+        // Recursively copy next sibling nodes
+        if (originalNode.getNext() != null) {
+            Node<Integer> copiedNext = copyTree(originalNode.getNext());
+            copiedNode.setNext(copiedNext);
+        }
+
+        return copiedNode;
     }
 
     @Override
     public void clean() {
-
+        deleteLayerWithChildren(0);
     }
 
     @Override
     public void print() {
-        System.out.println(traversePreOrder(getRoot()));
-    }
-
-    private String traversePreOrder(Node<Integer> root) {
-
-        if (root == null) {
-            return "List is empty";
+        if (getRoot() == null) {
+            System.out.println("Multi-list is empty");
         }
 
-        StringBuilder sb = new StringBuilder();
-        sb.append(root.getValue());
-
-        String pointerRight = "└──";
-        String pointerLeft = (root.getNext() != null) ? "├──" : "└──";
-
-        traverseNodes(sb, "", pointerLeft, root.getChild(), root.getNext() != null);
-        traverseNodes(sb, "", pointerRight, root.getNext(), false);
-
-        return sb.toString();
+        AtomicInteger index = new AtomicInteger(0);
+        printTree(getRoot(), "", index);
     }
 
-    private void traverseNodes(StringBuilder sb, String padding, String pointer, Node<Integer> node,
-                               boolean hasRightSibling) {
-        if (node != null) {
-            sb.append("\n");
-            sb.append(padding);
-            sb.append(pointer);
-            sb.append(node.getValue());
+    private void printTree(Node<Integer> node, String indent, AtomicInteger index) {
+        ;
+        if (node == null) {
+            return;
+        }
 
-            StringBuilder paddingBuilder = new StringBuilder(padding);
-            if (hasRightSibling) {
-                paddingBuilder.append("│  ");
-            } else {
-                paddingBuilder.append("   ");
-            }
+        // Print the current node's value
+        System.out.println(indent + index.incrementAndGet() + ":" + node.getValue());
 
-            String paddingForBoth = paddingBuilder.toString();
-            String pointerRight = "└──";
-            String pointerLeft = (node.getNext() != null) ? "├──" : "└──";
+        // Recursively print child nodes with increased indentation
+        if (node.getChild() != null) {
+            printTree(node.getChild(), indent + "    ", index);
+        }
 
-            traverseNodes(sb, paddingForBoth, pointerLeft, node.getChild(), node.getNext() != null);
-            traverseNodes(sb, paddingForBoth, pointerRight, node.getNext(), false);
+        // Recursively print next sibling nodes at the same level
+        if (node.getNext() != null) {
+            printTree(node.getNext(), indent, index);
         }
     }
+
 
     private Node<Integer> traversal(int index, Node<Integer> current, AtomicInteger count) {
         if (!Objects.equals(index, count.get())) {
@@ -139,7 +233,9 @@ public class MultiList implements GenericTree<Integer> {
         if (current.getChild() != null) {
             node = traversal(index, current.getChild(), count);
         }
-
+        if (Objects.equals(index, count.get())) {
+            return node;
+        }
         if (current.getNext() != null) {
             node = traversal(index, current.getNext(), count);
         }
