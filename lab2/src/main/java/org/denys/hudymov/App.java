05c0c21eb7fc740d3c881edc2869bee04cb8bc8d
Denys <denys.hudymov@gmail.com>
2023-10-19 23:53:09 +0300

    Main class for running console interface for a Skip List

---
+++ lab2/src/main/java/org/denys/hudymov/App.java
@@ -0,0 +1,8 @@
+package org.denys.hudymov;
+
+public class App {
+    public static void main(String[] args) {
+        ConsoleInterface consoleInterface = new ConsoleInterface();
+        consoleInterface.run();
+    }
+}
